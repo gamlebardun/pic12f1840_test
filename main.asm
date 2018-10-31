103e5d40be83930723349a3d2705f762bc6fd0b4
Ketil Duna <ketil.duna@gmail.com>
2018-10-31 18:29:22 +0100

    Update main.asm
    fixed several issues - now works as expected

--- main.asm
+++ main.asm
@@ -1,10 +1,15 @@
-#include <p12f1840.inc>
+#include <p12f1840.inc> ; suitable constants for this chip
+    
+; my chosen config was built using "Set configuration bits" in mplab x ide
+; to suit this project. The options should be self-explaining
+    
 ; CONFIG1
-; __config 0xC9A4
- __CONFIG _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _PWRTE_ON & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_ON & _CLKOUTEN_ON & _IESO_ON & _FCMEN_ON
+; __config 0x3F84
+ __CONFIG _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _PWRTE_ON & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_ON & _CLKOUTEN_OFF & _IESO_ON & _FCMEN_ON
 ; CONFIG2
-; __config 0xDEFF
+; __config 0x1EFF
  __CONFIG _CONFIG2, _WRT_OFF & _PLLEN_OFF & _STVREN_ON & _BORV_LO & _LVP_OFF
+
 
 ; Constants
   ; no PLL, 4MHz clock, internal clock source
@@ -13,27 +18,20 @@ OSCCONBITS EQU (0 << SPLLEN) | (b'1011' << IRCF0) | (b'10' << SCS0)
 ANSELBITS EQU b'00010100' ; buggers are all on by default (!)
   ; a single output pin for led management
 TRISBITS EQU b'111100' ; 0=output, 1=input (high impedance)
-  ; down the comparator
-CM1CON0BITS EQU b'10000000'  ; off
-CM1CON1BITS EQU b'00100000'  ; no interrupts  
   
 T1CONBITS EQU b'00110001' ; 1/8 prescale, run ..  
 
-cblock 0x20
-  INNER
-  OUTER
-endc
+  cblock 0x70
+    INNER
+    OUTER
+  endc
  
-; TODO PLACE VARIABLE DEFINITIONS GO HERE
-
 RVECT  code    0x0000        ; processor reset vector
   goto    SETUP
 
 ISR    code    0x0004        ; interrupts go here
   goto    INTHANDLER
 
-THECODE code                      ; no adress, linker places main program
- 
 SETUP 
 
   ; setting it up .. configure clock, io and peripherals
@@ -45,11 +43,6 @@ SETUP
   ; peripherals are in general disabled, no trouble on startup
   ; but the comparator can be tied down if not used for even 
   ; less power usage
-  movlb 2 ; comparator bank
-  movlw CM1CON0BITS
-  movwf CM1CON0
-  movlw CM1CON1BITS
-  movwf CM1CON1
   movlb 3 ; ANSEL resides here ..
   movlw ANSELBITS
   movwf ANSELA
@@ -88,8 +81,8 @@ INTHANDLER
   movlb 0
   bcf PIR1, TMR1IF ; clear existing interrupt flag
   movlb 2
-  movlw 1          ; flipt lower bit
-  xorwf LATA, f    ; in the latch (read = guaranteed previous set value)
+  movlw b'00000001'; prepare to flip a bit
+  xorwf LATA, f    ; flip output of bit 0, RA0, pin 7
   
   retfie           ; auto state change on interrupt - just leave when done
   
